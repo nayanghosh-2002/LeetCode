5e936f6c42a794710a117c7ea2ca295cdf4aff2b
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-06-10 21:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (68.21%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        
-        int mxOdd = 0;
-        int mnEven = 102;
-        int n = s.length();
-        vector<int> charCnt(26, 0);
-
-        for(int i = 0; i < n; i++) {
-            int idx = s[i] - 'a';
-            charCnt[idx]++;
-        }
-
-        for(int i = 0; i < 26; i++) {
-
-            if(charCnt[i] == 0) continue;
-
-            if(charCnt[i] % 2 == 0) {
-                mnEven = min(charCnt[i], mnEven);
-            } else {
-                mxOdd = max(charCnt[i], mxOdd);
-            }
-        }
-
-        return mxOdd - mnEven;
+        vector<int> mpp(26);
+        int maxi = 0, mini = s.size();
+        for (char c : s) mpp[c - 'a']++;
+        for (int i = 0 ; i < 26; i++) {
+            if (mpp[i] % 2 != 0) maxi = max(maxi, mpp[i]);
+            if (mpp[i] % 2 == 0 && mpp[i] > 0) mini = min(mini, mpp[i]);
+        } 
+        return maxi - mini;
     }
 };
